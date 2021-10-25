89658cfc706880a450e52151605a89cbab5dbc51
gaianacheli <gaianacheli04@gmail.com>
2021-10-25 12:20:27 +0200

    snack 2

---
+++ snack-1/js/main.js
@@ -0,0 +1,26 @@
+
+const biciCorsa = [
+{
+    nameBici: 'Passoni',
+    peso: '6,5 Kg',
+},
+{
+    nameBici: 'Freccia',
+    peso: '7,5 Kg',
+},
+{
+    nameBici: 'Pirandello',
+    peso: '8,5 Kg',
+}
+]
+let variabileAppoggio = biciCorsa[0]
+
+for (let i = 0 ; i < biciCorsa.length; i++) {
+ const bici = biciCorsa[i];
+
+  if(bici.peso < variabileAppoggio.peso){
+    variabileAppoggio = bici
+  };
+}
+console.log(variabileAppoggio);
+document.getElementById('.risultato').innerHTML = 'la bici piu leggera è :' ${variabileAppoggio.nameBici} 'e pesa:' ${variabileAppoggio.peso}
---
+++ snack-2/js/main.js
@@ -0,0 +1,23 @@
+const squadreCalcio = [
+    {
+        name: 'inter',
+        punti: '0',
+        falli:'0'
+
+    },
+    {
+        name: 'milan',
+        punti: '0',
+        falli:'0'
+    },
+    {
+        name: 'roma',
+        punti: '0',
+        falli:'0'
+    },
+    {
+        name: 'torino',
+        punti: '0',
+        falli:'0'
+    },
+]

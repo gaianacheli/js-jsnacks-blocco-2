388786b675cf2f8d6c45c922f903b47c097a9b47
gaianacheli <gaianacheli04@gmail.com>
2021-10-25 12:31:14 +0200

    snack 2 sistemato

--- snack-2/js/main.js
+++ snack-2/js/main.js
@@ -1,23 +1,23 @@
 const squadreCalcio = [
     {
         name: 'inter',
-        punti: '0',
-        falli:'0'
+        punti: 0,
+        falli: 0
 
     },
     {
         name: 'milan',
-        punti: '0',
-        falli:'0'
+        punti: 0,
+        falli: 0
     },
     {
         name: 'roma',
-        punti: '0',
-        falli:'0'
+        punti: 0,
+        falli: 0
     },
     {
         name: 'torino',
-        punti: '0',
-        falli:'0'
+        punti: 0,
+        falli: 0
     },
 ]
